2bcb8090bd35e2b91e3ff1589ef268e96225a19b
Sebas <sebassanchezc1@gmail.com>
2026-04-30 11:16:39 -0500

    push inicial

---
+++ taller4/src/test/java/pa/microservicios/taller4/Taller4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pa.microservicios.taller4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
