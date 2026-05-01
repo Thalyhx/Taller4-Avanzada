55bec742cec45fc5e97aee8f80b7139e40a14126
Nath <nathalycis20@gmail.com>
2026-05-01 18:48:46 -0500

    Añadiendo CORS

--- Taller4-BackEnd/src/main/java/edu/udistrital/BackEnd/Taller4Application.java
+++ Taller4-BackEnd/src/main/java/edu/udistrital/BackEnd/Taller4Application.java
@@ -3,14 +3,35 @@ package edu.udistrital.BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
+@Configuration
 public class Taller4Application {
     
     public static void main(String[] args) {
 	SpringApplication.run(Taller4Application.class, args);
     
     }
+    
+    @Bean 
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+    
+    
        
   }
