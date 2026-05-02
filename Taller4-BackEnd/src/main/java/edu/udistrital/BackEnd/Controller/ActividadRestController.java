c1b181f87c6670d2fabb42a1dc9e849acc440b50
Nath <nathalycis20@gmail.com>
2026-05-02 11:19:07 -0500

    añadiendo Controller

--- Taller4-BackEnd/src/main/java/edu/udistrital/BackEnd/Controller/ActividadRestController.java
+++ Taller4-BackEnd/src/main/java/edu/udistrital/BackEnd/Controller/ActividadRestController.java
@@ -4,8 +4,21 @@
  */
 package edu.udistrital.BackEnd.Controller;
 
-import edu.udistrital.BackEnd.Repository.ActividadRepository;
+import edu.udistrital.BackEnd.Model.ActividadDTO;
 import edu.udistrital.BackEnd.Service.ActividadService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,21 +27,79 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/api/actividad")
 public class ActividadRestController {
     
     //Inyeccion a Actividadservice
+     /**
+     * Constructor con inyección de dependencias
+     * @param service Instancia del servicio
+     */
+    @Autowired
     public ActividadService service;
     public ActividadRestController(ActividadService service) {
-        this.service = service;
+    this.service = service;
+    }
+    
+     /**
+     * Crea una nueva actividad
+     * @param actividadDTO Objeto Actividad a crear (recibido como JSON)
+     * @return ResponseEntity con la actividad creada
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ActividadDTO crearActividad(@RequestBody ActividadDTO actividad) {
+  
+    return service.crear(actividad);
+    }
+    
+     /**
+     *  Obtener todas las actividades
+     */
+    @GetMapping("/actividades")
+    public List<ActividadDTO> obtenerActividades() {
+         
+    return service.obtenerTodas();
+    }
+    
+     /**
+     * Obtener una actividad por ID
+     * @param id ID de la actividad
+     */
+    @GetMapping("/{id}")
+    public ActividadDTO obtenerActividadPorId(@PathVariable("id") Long id) {
+
+        return service.obtenerPorId(id);
+
+    }
+    
+     /**
+     * PActualizar una actividad existente
+     * @param id ID de la actividad a actualizar
+     * @param actividadActualizada Datos actualizados de la actividad (recibido como JSON)
+     */
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ActividadDTO> actualizarActividad(
+                @PathVariable Long id, 
+                @RequestBody ActividadDTO actividadActualizada) {
+
+            // Llamamos al servicio que devuelve el Optional
+            Optional<ActividadDTO> resultado = service.actualizar(id, actividadActualizada);
+            return resultado
+                    .map(actividad -> ResponseEntity.ok(actividad)) 
+                .orElseGet(() -> ResponseEntity.notFound().build());
+                }
+        
+    
+     /**
+     * Eliminar una actividad por ID
+     * @param id ID de la actividad a eliminar
+     */
+    @DeleteMapping("/delete/{id}")
+    public void eliminarActividad(@PathVariable("id") Long id) {
+
+        service.eliminar(id);
+        
     }
 
-    
-    //Inyeccion a Actividadrepositorio
-    ActividadRepository repository;
-    public ActividadRestController(ActividadRepository repository){
-        this.repository = repository;
-    }    
-    
-    
-  
 }
