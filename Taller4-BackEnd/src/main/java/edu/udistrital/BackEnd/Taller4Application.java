a675943f896f4c1bd14912f7be5a661691f3bc96
Nath <nathalycis20@gmail.com>
2026-05-02 13:56:27 -0500

    Añadiendo modificaciones

--- Taller4-BackEnd/src/main/java/edu/udistrital/BackEnd/Taller4Application.java
+++ Taller4-BackEnd/src/main/java/edu/udistrital/BackEnd/Taller4Application.java
@@ -4,7 +4,6 @@ package edu.udistrital.BackEnd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
